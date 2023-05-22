a4493cb329487b704a967d56fef0f69a383554b1
kajal <72293433+kajal1544@users.noreply.github.com>
2023-05-22 10:20:39 +0530

    Add files via upload

--- pythonmqtt/Subscriber.py
+++ pythonmqtt/Subscriber.py
@@ -5,18 +5,15 @@ broker_address = "localhost"
 broker_port = 1883
 topic = "ks"
 
-
 # Callback function for when the subscriber connects to the broker
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: " + str(rc))
     # Subscribe to the topic
     client.subscribe(topic)
 
-
 # Callback function for when the subscriber receives a message
 def on_message(client, userdata, message):
     print("Received message: " + str(message.payload.decode()))
-
 
 # Create MQTT client and set the callback functions
 client = mqtt.Client()
